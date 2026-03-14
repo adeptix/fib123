612c36f9b3ffd61104cd056a104dab3ec5d455a3
adeptix <adeptgoo@gmail.com>
2026-03-14 14:27:44 +0400

    feat: IsNFib function

--- fib.go
+++ fib.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Fib returns n fibonacci number
 func Fib(n int) int {
 	a := 0
 	b := 1
@@ -15,6 +16,7 @@ func Fib(n int) int {
 	return a
 }
 
+// PrintFibs prints fibonacci numbers
 func PrintFibs(count int) {
 	if count < 1 {
 		return
@@ -27,4 +29,9 @@ func PrintFibs(count int) {
 		fmt.Println(a)
 		a, b = b, a+b
 	}
+}
+
+// IsNFib checks if number is n-th fibonacci number. See Fib
+func IsNFib(n int, number int) bool {
+	return Fib(n) == number
 }
