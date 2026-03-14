d86c4f51f76179c6bad0ca6959bc4454e6049c4f
adeptix <adeptgoo@gmail.com>
2026-03-14 13:38:09 +0400

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adeptix/fib123
+
+go 1.25.1
---
+++ negative/fib.go
@@ -0,0 +1,28 @@
+package negative
+
+import "fmt"
+
+func Fib(n int) int {
+	a := 0
+	b := -1
+
+	for range n {
+		a, b = b, a+b
+	}
+
+	return a
+}
+
+func PrintFibs(count int) {
+	if count < 1 {
+		return
+	}
+
+	a := 0
+	b := -1
+
+	for range count {
+		fmt.Println(a)
+		a, b = b, a+b
+	}
+}
